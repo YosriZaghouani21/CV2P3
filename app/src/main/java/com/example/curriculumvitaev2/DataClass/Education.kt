5f769824589a0d67cb228a254a1be5b6faaa5401
Yosrii <zaghouani.yosri@gmail.com>
2022-10-23 12:44:24 +0100

    Update

---
+++ app/src/main/java/com/example/curriculumvitaev2/DataClass/Education.kt
@@ -0,0 +1,8 @@
+package com.example.curriculumvitaev2.DataClass
+
+data class Education(
+    val name: String,
+    val adress: String,
+    val endDate: String,
+    val startDate:String,
+    val img: Int)
---
+++ app/src/main/java/com/example/curriculumvitaev2/DataClass/Experience.kt
@@ -0,0 +1,9 @@
+package com.example.curriculumvitaev2.DataClass
+
+data class Experience(
+    val name: String,
+    val adress: String,
+    val endDate: String,
+    val description: String,
+    val img: Int,
+    val startDate:String)
